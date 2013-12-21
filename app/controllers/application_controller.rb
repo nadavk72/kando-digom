e0106912e3d7e1c89dfca33e45e97dd3298fbfb5
Nadav Karpenkop <nadavk72@gmail.com>
2013-12-21 22:58:40 +0200

    sfsff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   
-  before_filter :force_mobile_html
-  has_mobile_fu
+  #before_filter :force_mobile_html
+  #has_mobile_fu
 
-  def force_mobile_html
-    session[:moblie_view] = false
-  end
+  #def force_mobile_html
+  #  session[:moblie_view] = false
+  #end
   
   protect_from_forgery with: :exception
 
